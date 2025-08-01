0d5d5e6fcf9945adef5b6aa240a9cb1fe95ea70f
rdionis <dionisio.raquel@gmail.com>
2025-08-01 17:43:52 +0200

    intro to CRUD

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,8 +1,32 @@
+import e from "express";
 import express from "express";
 
 import { check, validationResult } from "express-validator";
 
 const router = express.Router();
+
+const validations = [
+  check("name")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("A name is required."),
+  check("email")
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("A valid email adress is required."),
+  check("title")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("A title is required."),
+  check("message")
+    .trim()
+    .isLength({ min: 5 })
+    .escape()
+    .withMessage("A message is required."),
+];
 
 export default params => {
   const { feedbackService } = params;
@@ -11,7 +35,9 @@ export default params => {
     try {
       const feedback = await feedbackService.getList();
 
-      const errors = request.session.feedback ? request.session.feedback.errors : false;
+      const errors = request.session.feedback
+        ? request.session.feedback.errors
+        : false;
 
       const successMessage = request.session.feedback
         ? request.session.feedback.message
@@ -31,31 +57,8 @@ export default params => {
     }
   });
 
-  router.post(
-    "/",
-    [
-      check("name")
-        .trim()
-        .isLength({ min: 3 })
-        .escape()
-        .withMessage("A name is required."),
-      check("email")
-        .trim()
-        .isEmail()
-        .normalizeEmail()
-        .withMessage("A valid email adress is required."),
-      check("title")
-        .trim()
-        .isLength({ min: 3 })
-        .escape()
-        .withMessage("A title is required."),
-      check("message")
-        .trim()
-        .isLength({ min: 5 })
-        .escape()
-        .withMessage("A message is required."),
-    ],
-    async (request, response) => {
+  router.post("/", validations, async (request, response, next) => {
+    try {
       const errors = validationResult(request);
       if (!errors.isEmpty()) {
         request.session.feedback = {
@@ -63,16 +66,31 @@ export default params => {
         };
         return response.redirect("/feedback");
       }
+    } catch (error) {
+      next(error);
+    }
 
+    const { name, email, title, message } = request.body;
+    await feedbackService.addEntry(name, email, title, message);
+    request.session.feedback = {
+      message: "Thank you for your feedback!",
+    };
+    // return response.send("Feedback form posted.");
+    return response.redirect("/feedback");
+  });
+
+  router.post("/api", validations, async (request, response, next) => {
+    try {
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        return response.json({ errors: errors.array() });
+      }
       const { name, email, title, message } = request.body;
       await feedbackService.addEntry(name, email, title, message);
-      request.session.feedback = {
-        message: "Thank you for your feedback!",
-      };
-      // return response.send("Feedback form posted.");
-      return response.redirect("/feedback");
+    } catch (error) {
+      next(error);
     }
-  );
+  });
 
   return router;
 };
